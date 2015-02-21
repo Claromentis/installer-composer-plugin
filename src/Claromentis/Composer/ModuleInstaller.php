c33f55ecc19c0fc29998dcbfc3b339e7ad4599fe
Alexander Polyanskikh <alexander.polyanskikh@claromentis.com>
2015-02-21 23:15:58 +0000

    Added uninstall

--- src/Claromentis/Composer/ModuleInstaller.php
+++ src/Claromentis/Composer/ModuleInstaller.php
@@ -1,8 +1,12 @@
 <?php
 namespace Claromentis\Composer;
 
+use Composer\Composer;
 use Composer\Installer\LibraryInstaller;
+use Composer\IO\IOInterface;
 use Composer\Package\PackageInterface;
+use Composer\Repository\InstalledRepositoryInterface;
+use Composer\Util\Filesystem;
 
 class ModuleInstaller extends LibraryInstaller
 {
@@ -18,5 +22,18 @@ class ModuleInstaller extends LibraryInstaller
 			return 'web/';
 
 		return 'web/intranet/' . $parts[1] . '/';
+	}
+
+	public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package)
+	{
+		if (!$repo->hasPackage($package)) {
+			throw new \InvalidArgumentException('Package is not installed: '.$package);
+		}
+
+		$repo->removePackage($package);
+
+		$installPath = $this->getInstallPath($package);
+		$removeResult = $this->filesystem->removeDirectory($installPath);
+		$this->io->write(sprintf('Deleting %s - %s', $installPath, $removeResult ? '<comment>deleted</comment>' : '<error>not deleted</error>'));
 	}
 }
