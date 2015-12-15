9a9eb9469d8f264e5b5f8e5b26682fa2784be17f
Alexander Polyanskikh <alexander.polyanskikh@claromentis.com>
2015-12-15 17:16:02 +0000

    Added core installer v8

---
+++ src/Claromentis/Composer/FrameworkInstallerV8.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Claromentis\Composer;
+
+use Composer\DependencyResolver\Operation\InstallOperation;
+use Composer\DependencyResolver\Operation\UpdateOperation;
+use Composer\Package\PackageInterface;
+use Composer\Repository\InstalledRepositoryInterface;
+
+/**
+ * Installer for pre-composer modules - those that have distributives as zip files
+ * with application directory in it and probably no composer.json
+ *
+ * @package Claromentis\Composer
+ */
+class FrameworkInstallerV8 extends BaseInstaller
+{
+	public function supports($packageType)
+	{
+		return $packageType === 'claromentis-framework-v8';
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
+	{
+		$this->installCode($package);
+		if (!$repo->hasPackage($package)) {
+			$repo->addPackage(clone $package);
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
+	{
+		$this->updateCode($repo, $initial, $target);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function uninstall(InstalledRepositoryInterface $repo, PackageInterface $package)
+	{
+		if (!$repo->hasPackage($package)) {
+			throw new \InvalidArgumentException('Package is not installed: '.$package);
+		}
+
+		//$this->removeCode($package);
+		$repo->removePackage($package);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getInstallPath(PackageInterface $package)
+	{
+		return '.';
+	}
+
+	protected function removeCode(PackageInterface $package)
+	{
+		$downloadPath = $this->getInstallPath($package);
+		$this->downloadManager->remove($package, $downloadPath);
+		$this->filesystem->removeDirectory($downloadPath);
+	}
+
+	public function onInstall(InstallOperation $operation)
+	{
+		if ($this->CoreConfigExists())
+		{
+			$this->io->write('<warning>Core config file already exists, so assuming this is an upgrade from pre-installer version. Running "phing upgrade"');
+			$this->runPhing('core', 'upgrade');
+		} else
+		{
+			$this->runPhing('core', 'install');
+		}
+	}
+
+	public function onUpdate(UpdateOperation $operation)
+	{
+		$this->runPhing('core', 'upgrade');
+	}
+
+	private function CoreConfigExists()
+	{
+		return file_exists('web/intranet/common/config.php');
+	}
+}
